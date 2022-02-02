b4560108f962c9c8882a0597c80c6aef8639a17a
ElenaKars <87471171+ElenaKars@users.noreply.github.com>
2022-02-02 23:43:14 +0800

    updated task 1 task.js

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,61 +1,32 @@
-// v1
 const mainModalEl = document.querySelector("#modal_main");
 const successModalEl = document.querySelector("#modal_success");
 
+const closeBtnsEls = document.querySelectorAll(".modal__close");
+const goodBtnsEls = document.querySelectorAll(".btn");
+
+const toDoGoodBtnEl = document.querySelector(".show-success");
+
 mainModalEl.classList.add("modal_active");
 
-const mainModalCloseBtnEl = mainModalEl.querySelector(".modal__close");
-mainModalCloseBtnEl.addEventListener("click", () => {
-  mainModalEl.classList.remove("modal_active");
+const allBtnsArr = [...closeBtnsEls, ...goodBtnsEls];
+
+allBtnsArr.forEach((el) => {
+  el.addEventListener("click", function (evt) {
+    const targetEl = evt.target;
+    if (allBtnsArr.includes(targetEl)) {
+      this.closest(".modal").classList.remove("modal_active");
+    }
+  });
 });
 
-const mainModalDoGoodBtnEl = mainModalEl.querySelector(".btn");
-mainModalDoGoodBtnEl.addEventListener("click", () => {
-  mainModalEl.classList.remove("modal_active");
+toDoGoodBtnEl.addEventListener("click", () => {
   successModalEl.classList.add("modal_active");
 });
 
-const successModalCloseBtnEl = successModalEl.querySelector(".modal__close");
-successModalCloseBtnEl.addEventListener("click", () => {
-  successModalEl.classList.remove("modal_active");
+document.addEventListener("keydown", (evt) => {
+  if (evt.key === "Escape") {
+    document.querySelector(".modal_active").classList.remove("modal_active");
+  }
 });
 
-const successModalGoodBtnEl = successModalEl.querySelector(".btn");
-successModalGoodBtnEl.addEventListener("click", () => {
-  successModalEl.classList.remove("modal_active");
-});
-
-
-//закрытие по клику за пределами модального окна && при нажатии Esc
-
-// const modalsEls = document.querySelectorAll(".modal");
-// const mainModalEl = document.querySelector("#modal_main");
-// const successModalEl = document.querySelector("#modal_success");
-
-// const closeBtnsEls = document.querySelectorAll(".modal__close");
-// const goodBtnsEls = document.querySelectorAll(".btn");
-// const successBtnEl = document.querySelector(".show-success");
-
-// mainModalEl.classList.add("modal_active");
-
-// const coloseElsArr = [...closeBtnsEls, ...goodBtnsEls, ...modalsEls];
-
-// coloseElsArr.forEach((el) => {
-//   el.addEventListener("click", (evt) => {
-//     const targetEl = evt.target;
-//     if (coloseElsArr.includes(targetEl)) {
-//       el.closest(".modal").classList.remove("modal_active");
-//     }
-//   });
-// });
-
-// successBtnEl.addEventListener("click", () => {
-//   successModalEl.classList.add("modal_active");
-// });
-
-// document.addEventListener("keydown", (evt) => {
-//   if (evt.key === "Escape") {
-//     document.querySelector(".modal_active").classList.remove("modal_active");
-//   }
-// });
 
