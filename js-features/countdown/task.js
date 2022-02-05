ea9854e53dfc214058e37f0dcc99fbda8bb63cff
ElenaKars <87471171+ElenaKars@users.noreply.github.com>
2022-02-05 19:40:16 +0800

    updated task 1 task.js

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,11 +1,54 @@
-const updateCountdown = function(){
-	const timer = document.getElementById("timer");
-	if(timer.textContent > 0){
-		timer.textContent--;
-	}
-	else{
-		alert("Вы победили в конкурсе!")
-	}	
+const intervalId = setInterval(updateCountdown, 1000);
+
+const hoursEl = document.getElementById("hh");
+const minutesEl = document.getElementById("mm");
+const secondsEl = document.getElementById("ss");
+
+function updateCountdown() {
+  const hours = +hoursEl.textContent;
+  const minutes = +minutesEl.textContent;
+  const seconds = +secondsEl.textContent;
+
+  const totalSeconds = seconds + minutes * 60 + hours * 60 * 60;
+
+  if (totalSeconds === 0) {
+    alert("Время вышло!");
+    clearInterval(intervalId);
+
+    return;
+  }
+
+  const newTotalSeconds = totalSeconds - 1;
+
+  const { hh, mm, ss } = getTime(newTotalSeconds);
+
+  hoursEl.textContent = hh;
+  minutesEl.textContent = mm;
+  secondsEl.textContent = ss;
 }
 
-setInterval(updateCountdown, 1000);
+function getTime(seconds) {
+  const hh = Math.trunc(seconds / (60 * 60));
+  const hhInSec = hh * 60 * 60;
+
+  const mm = Math.trunc((seconds - hhInSec) / 60);
+  const mmInSec = mm * 60;
+
+  const ss = seconds - (hhInSec + mmInSec);
+
+  return {
+    hh: leadingZero(hh),
+    mm: leadingZero(mm),
+    ss: leadingZero(ss)
+  };
+}
+
+function leadingZero(number) {
+  const result = String(number);
+
+  if (result.length === 1) {
+    return "0" + result;
+  }
+
+  return result;
+}
